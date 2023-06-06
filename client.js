0632058507928744c21521ac5ff800011d19a0f6
Sam McCauley <sammccauley34@gmail.com>
2023-06-06 16:13:37 -0500

    commit running copy

--- client.js
+++ client.js
@@ -43,15 +43,55 @@ console.log('array of employee data: ',  employees );
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-
-
+/*
+{
+  name: 'Atticus',
+  employeeNumber: '2405',
+  annualSalary: '47000',
+  reviewRating: 3
+},
+*/
 
 // This function will calculate 1 employee's bonus!
-//
+
 function calculateIndividualEmployeeBonus( employee ) {  
+  let bonusPercentage = 0;
   // your logic here
-  
-  
+  if(employee.reviewRating <= 2){
+    bonusPercentage = 0
+    console.log( 'current bonus percentage:', bonusPercentage);
+  }
+
+    if(employee.reviewRating === 3){
+    bonusPercentage = .04;
+    }
+    if(employee.reviewRating === 4){
+      bonusPercentage = .06;
+    }
+    if(employee.reviewRating === 5){
+      bonusPercentage = .10; 
+    }
+
+    else{
+    }
+
+    return result;
+  }
   // return new object with bonus results
+  
+  let result = {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: 'totalCompensation',
+    totalBonus: 'totalBonus'
+    }
+
+//console.log( 'call function to test for Atticus:', calculateIndividualEmployeeBonus(employees[0]) );
+
+console.log( 'call function to test for Atticus:', calculateIndividualEmployeeBonus( employees[0] ));
+
+for(let individuals of employees){
+  console.log(`${individual.name}`, 'color: black; background: yellow;')
+  calculateIndividualEmployeeBonus
 
 }
